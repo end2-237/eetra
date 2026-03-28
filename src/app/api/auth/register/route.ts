b5b6276bb329a305addcaabc218861877f95908c
Your Name <you@example.com>
2026-03-28 15:06:18 +0100

    fix temId

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -34,6 +34,7 @@ export async function POST(req: NextRequest) {
             email: email.toLowerCase(),
             role: 'admin',
             avatar: '👑',
+            teamId: ""
           },
         },
       },
